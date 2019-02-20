7d80cf4bd27f2e9f7432b66acf49e480cf17c732
Akshaya Rajagopalan <a2rajago@edu.uwaterloo.ca>
2019-02-20 13:49:17 -0500

    Seperating into vue components

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-const app = new Vue({
-    el: "#app",
-    data: {
-        friends: [
-            {
-                first: "Bobby",
-                last: "Boone",
-                age: 25
-            },
-            {
-                first: "John",
-                last: "Boone",
-                age: 35
-            }
-        ],
-    },
+Vue.component('friend-component', {
+    props: ['friend'],
     filters: {
         ageInOneYear(value) {
             return value.age + 1;
@@ -32,14 +18,37 @@ const app = new Vue({
     },
     template: `
         <div>
-            <h2 v-for="friend in friends">
-                <h4>{{friend | fullName}}</h4>
-                <h5>{{friend.age}}</h5>
-                <button v-on:click="decrementAge(friend)">-</button>
-                <button v-on:click="incrementAge(friend)">+</button>
-                <input v-model="friend.first"/>
-                <input v-model="friend.last"/>
-            </h2>
+            <h4>{{friend | fullName}}</h4>
+            <h5>{{friend.age}}</h5>
+            <button v-on:click="decrementAge(friend)">-</button>
+            <button v-on:click="incrementAge(friend)">+</button>
+            <input v-model="friend.first"/>
+            <input v-model="friend.last"/>
+        </div>
+    `
+});
+
+
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        friends: [
+            {
+                first: "Bobby",
+                last: "Boone",
+                age: 25
+            },
+            {
+                first: "John",
+                last: "Boone",
+                age: 35
+            }
+        ],
+    },
+    template: `
+        <div>
+            <friend-component v-for="item in friends" v-bind:friend="item" />
         </div>
     `
 })
