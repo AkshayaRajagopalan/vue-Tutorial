cf9507ec255a45a44d48fc6acc9e01107ef82c5d
Akshaya Rajagopalan <a2rajago@edu.uwaterloo.ca>
2019-02-20 14:11:32 -0500

    LifeCycle Methods

--- main.js
+++ main.js
@@ -1,54 +1,18 @@
-Vue.component('friend-component', {
-    props: ['friend'],
-    filters: {
-        ageInOneYear(value) {
-            return value.age + 1;
-        },
-        fullName(value){
-            return `${value.last}, ${value.first}`;
-        }
-    },
-    methods: {
-        incrementAge(friend) {
-            friend.age = friends.age+1;
-        },
-        decrementAge(friend) {
-            friend.age = friends.age-1;
-        }
-    },
-    template: `
-        <div>
-            <h4>{{friend | fullName}}</h4>
-            <h5>{{friend.age}}</h5>
-            <button v-on:click="decrementAge(friend)">-</button>
-            <button v-on:click="incrementAge(friend)">+</button>
-            <input v-model="friend.first"/>
-            <input v-model="friend.last"/>
-        </div>
-    `
-});
-
-
-
 const app = new Vue({
     el: "#app",
     data: {
-        friends: [
-            {
-                first: "Bobby",
-                last: "Boone",
-                age: 25
-            },
-            {
-                first: "John",
-                last: "Boone",
-                age: 35
-            }
-        ],
+        friends: [],
+    },
+    mounted() {
+        fetch("http://rest.learncode.academy/api/vue/friends")
+        .then(response => response.json())
+        .then((data) => {
+            this.friends = data;
+        })
     },
     template: `
         <div>
-            <friend-component v-for="item in friends" v-bind:friend="item" />
+            <li v-for="item in friends">{{friend.name}}<li/>
         </div>
     `
 })
