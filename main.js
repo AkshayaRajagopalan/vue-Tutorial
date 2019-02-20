76845b0bebc099e3c02a4b5ecb433412ffa82133
Akshaya Rajagopalan <a2rajago@edu.uwaterloo.ca>
2019-02-20 14:27:20 -0500

    Full REST example

--- main.js
+++ main.js
@@ -2,6 +2,29 @@ const app = new Vue({
     el: "#app",
     data: {
         friends: [],
+        editFriend: null,
+    },
+    methods: {
+        deleteFriend(id, i) {
+            fetch("http://rest.learncode.academy/api/vue/friends" + id, {
+                method: "DELETE"
+            })
+            .then(() => {
+                this.friends.splice(i, 1);
+            })
+        },
+        updateFriend(friend) {
+            fetch("http://rest.learncode.academy/api/vue/friends" + friend.id, {
+                body: JSON.stringify(friend),
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(() => {
+                this.editFriend = null;
+            })
+        }
     },
     mounted() {
         fetch("http://rest.learncode.academy/api/vue/friends")
@@ -12,7 +35,17 @@ const app = new Vue({
     },
     template: `
         <div>
-            <li v-for="item in friends">{{friend.name}}<li/>
+            <li v-for="friend, i in friends">
+                <div v-if="editFriend === friend.id">
+                    <input v-on:keyup.13="updateFriend(friend)" v-model="friend.name" />
+                    <button v-on:click="updateFriend(friend)">save</button>
+                </div>
+                <div v-else>
+                    <button v-on:click="editFriend = friend.id">edit</button>
+                    <button v-on:click="deleteFriend(friend.id, i)">x</button>
+                    {{friend.name}}
+                </div>
+            <li/>
         </div>
     `
 })
