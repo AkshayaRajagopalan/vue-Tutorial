8368eea7a1914d1615046cf49c0a96c1052d01c9
Akshaya Rajagopalan <a2rajago@edu.uwaterloo.ca>
2019-02-20 13:39:41 -0500

    directives and event listeners

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 const app = new VTTCue({
     el: "#app",
     data: {
-        bobby: {
-            first: "Bobby",
-            last: "Boone",
-            age: 25
-        },
-        john: {
-            first: "John",
-            last: "Boone",
-            age: 35
-        }
-    },
-    computed: {
-        johnAgeInOneYear(){
-            return this.john.age + 1;
-        }
+        friends: [
+            {
+                first: "Bobby",
+                last: "Boone",
+                age: 25
+            },
+            {
+                first: "John",
+                last: "Boone",
+                age: 35
+            }
+        ],
     },
     filters: {
         ageInOneYear(value) {
@@ -25,12 +22,24 @@ const app = new VTTCue({
             return `${value.last}, ${value.first}`;
         }
     },
+    methods: {
+        incrementAge(friend) {
+            friend.age = friends.age+1;
+        },
+        decrementAge(friend) {
+            friend.age = friends.age-1;
+        }
+    },
     template: `
         <div>
-            <h2>Name: {{john | fullName}}</h2>
-            <h2>Age: {{john | ageInOneYear}}</h2>
-            <h2>Name: {{bobby | fullName}}</h2>
-            <h2>Age: {{bobby | ageInOneYear}}</h2>
+            <h2 v-for="friend in friends">
+                <h4>{{friend | fullName}}</h4>
+                <h5>{{friend.age}}</h5>
+                <button v-on:click="decrementAge(friend)">-</button>
+                <button v-on:click="incrementAge(friend)">+</button>
+                <input v-model="friend.first"/>
+                <input v-model="friend.last"/>
+            </h2>
         </div>
     `
 })
